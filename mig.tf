37cfae9515c5363bfa562663ec4fec041a9f3d7d
Piotr Sacharuk <piotr.sacharuk@gmail.com>
2025-12-01 16:01:06 +0100

    Add subnetwork_project to instance template

--- mig.tf
+++ mig.tf
@@ -1,10 +1,11 @@
 module "vm_instance_template" {
-  source       = "terraform-google-modules/vm/google//modules/instance_template"
-  version      = "~> 13.6"
-  project_id   = var.project-id
-  region       = var.region
-  machine_type = "e2-micro"
-  subnetwork   = google_compute_subnetwork.subnet-1.name
+  source             = "terraform-google-modules/vm/google//modules/instance_template"
+  version            = "~> 13.6"
+  project_id         = var.project-id
+  region             = var.region
+  machine_type       = "e2-micro"
+  subnetwork         = google_compute_subnetwork.subnet-1.name
+  subnetwork_project = var.project-id
   service_account = {
     email  = var.instance_service_account
     scopes = []
