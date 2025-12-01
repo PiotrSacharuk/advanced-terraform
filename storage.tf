e949040bacf80ff7dce1dc86b3392b0b43f577d7
Piotr Sacharuk <piotr.sacharuk@gmail.com>
2025-12-01 14:23:55 +0100

    Enable uniform bucket level access for storage buckets

--- storage.tf
+++ storage.tf
@@ -1,8 +1,10 @@
 ## BUCKETS
 resource "google_storage_bucket" "environment-buckets" {
-  for_each = toset(var.environment_list)
-  name     = "${lower(each.key)}_${var.project-id}"
-  location = var.region
+  for_each                    = toset(var.environment_list)
+  name                        = "${lower(each.key)}_${var.project-id}"
+  location                    = var.region
+  uniform_bucket_level_access = true
+
   versioning {
     enabled = true
   }
